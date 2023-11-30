77897546f293e3e51ffcb41247cff7e73e0b061a
Sampath Gade <SampathGade>
2023-11-30 20:57:17 +0000

    push test sampath

--- mobileclientassetmanagement/src/AppExecute.java
+++ mobileclientassetmanagement/src/AppExecute.java
@@ -3,5 +3,6 @@ package mobileclientassetmanagement.src;
 public class AppExecute {
     public static void main(String[] args) {
            System.out.println("executed...");
+           System.out.print("Test Push Sampath");
     }
 }
