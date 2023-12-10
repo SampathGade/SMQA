f56ec558800cf4d3aaa8015e5fec825a37bd9905
lokeshdoorvasalu <lokeshdoorvasalu16@gmail.com>
2023-12-10 03:32:16 +0000

    Manage Maintenance Details

--- mobileclientassetmanagement/src/entity/maintenancerequest/MaintenanceRequestStatusImpl.java
+++ mobileclientassetmanagement/src/entity/maintenancerequest/MaintenanceRequestStatusImpl.java
@@ -1,6 +1,15 @@
-package mobileclientassetmanagement.src.entity.maintenancerequest;
+package entity.maintenancerequest;
+
+import status.Status;
 
 public class MaintenanceRequestStatusImpl extends Status {
- 
+    @Override
+    public void close(Object entity) {
+        MaintenanceRequest maintenanceRequest = (MaintenanceRequest) entity;
+        Integer requestID = maintenanceRequest.getRequestID();
+        Integer closedStatusCode = MaintenanceRequestStatus.CLOSED.getStatusCode();
+        maintenanceRequest.setRequestStatus(closedStatusCode);
+        MaintenanceRequestInterface maintenanceRequestInterface = new MaintenanceRequestFactoryImpl().createMaintenanceRequest();
+        maintenanceRequestInterface.update(requestID, maintenanceRequest);
+    }
 }
-
